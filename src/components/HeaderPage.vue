8fda0e3a28d397cb238a418f12b10f3e5478c02a
simulatedcode <simulasi.studio@gmail.com>
2022-12-06 19:26:13 +0700

    fix size header

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="max-w-3xl mx-auto pt-20">
+  <div class="max-w-2xl mx-auto pt-20">
     <div class="flex justify-between items-center">
       <div class="flex items-center space-x-2">
         <CarbonNotebook class="text-xl" />
