1cbfb5095857b3b0c581b487ed77ff848c7c1056
simulatedcode <simulasi.studio@gmail.com>
2022-12-08 10:59:55 +0700

    linter

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
-import Vue from "@vitejs/plugin-vue";
-import Components from "unplugin-vue-components/vite";
-import AutoImport from "unplugin-auto-import/vite";
-import Icons from "unplugin-icons/vite";
-import IconsResolver from "unplugin-icons/resolver";
+import { defineConfig } from 'vite';
+import Vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import AutoImport from 'unplugin-auto-import/vite';
+import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
 
 export default defineConfig({
   plugins: [
@@ -13,13 +13,13 @@ export default defineConfig({
       resolvers: [
         IconsResolver({
           prefix: false,
-          enabledCollections: ["carbon"],
+          enabledCollections: ['carbon'],
         }),
       ],
     }),
     AutoImport({
       dts: true,
-      imports: ["vue"],
+      imports: ['vue'],
     }),
     Icons({ autoInstall: true }),
   ],
